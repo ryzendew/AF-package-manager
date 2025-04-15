cb001eb7a1c86012547cbe8288931a7176206cdf
Mattscreative <84041391+ryzendew@users.noreply.github.com>
2025-04-15 00:49:43 -0300

    remove build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_pacmangui_FILE /home/matt/PacmanGui-new/build/pacmangui)
-set(__QT_DEPLOY_TARGET_pacmangui_TYPE EXECUTABLE)
--- build/CMakeFiles/pacmangui.dir/pacmangui_autogen/3YJK5W5UP7/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/pacmangui.dir/pacmangui_autogen/3YJK5W5UP7/qrc_resources.cpp.o: \
- /home/matt/PacmanGui-new/build/pacmangui_autogen/3YJK5W5UP7/qrc_resources.cpp \
- /usr/include/stdc-predef.h
