7792999fd9848bd5b37eaa5e27dd9da94e9befaa
Mattscreative <84041391+ryzendew@users.noreply.github.com>
2025-05-08 20:13:37 -0300

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_pacmangui_FILE /home/matt/AF-package-manager/build/pacmangui)
-set(__QT_DEPLOY_TARGET_pacmangui_TYPE EXECUTABLE)
--- build/CMakeFiles/pacmangui.dir/pacmangui_autogen/3YJK5W5UP7/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/pacmangui.dir/pacmangui_autogen/3YJK5W5UP7/qrc_resources.cpp.o: \
- /home/matt/AF-package-manager/build/pacmangui_autogen/3YJK5W5UP7/qrc_resources.cpp \
- /usr/include/stdc-predef.h
